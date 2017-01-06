f4a727f393c23478aef6484d61c288f64decdbb8
Ben Mossman <bdmossma@gmail.com>
2017-01-06 13:07:34 -0600

    added simple api examples demonstrating url params and url query strings

--- js/helloexpressjs/api_with_complex_url.js
+++ js/helloexpressjs/api_with_complex_url.js
@@ -4,11 +4,11 @@ var app = express()
 // API 1: API with URL params
 // Example URL: http://localhost:8080/apis/users/1/books/1
 app.get("/apis/users/:userId/books/:bookId", function(httpRequest, httpResponse) {
-	  var clientIp = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
+var clientIp = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
     console.log("Client " + clientIp + " is using API with URL params...");
     httpResponse.send(httpRequest.params);// sends {"userId":"1","bookId":"1"} to browser
 
-		if( "userId" && "bookId" in httpRequest.params) {
+    if( "userId" && "bookId" in httpRequest.params) {
         console.log("These URL params could be used to query data from a database for example.");
         console.log("userId: " + httpRequest.params.userId);
         console.log("bookId: " + httpRequest.params.bookId);
@@ -21,15 +21,15 @@ app.get("/apis/users/:userId/books/:bookId", function(httpRequest, httpResponse)
 // as it is a standard syntax that ExpressJS parses automatically
 // into an array of name-value pairs.
 app.get("/apis/books", function(httpRequest, httpResponse) {
-	  var clientIp = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
+    var clientIp = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
     console.log("Client " + clientIp + " is using API with URL query string...");
     httpResponse.send(httpRequest.query);// sends {"userId":"1","bookId":"1"} to browser
 
-		if( "userId" && "bookId" in httpRequest.query) {
+    if( "userId" && "bookId" in httpRequest.query) {
         console.log("These URL query string variables could be used to query data from a database for example.");
         console.log("userId: " + httpRequest.query.userId);
         console.log("bookId: " + httpRequest.query.bookId);
-		}
+    }
 });
 
 app.listen(8080);
