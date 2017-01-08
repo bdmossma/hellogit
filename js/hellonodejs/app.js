a8daaa1954d8b2b9708a07e1c4c7a4e84f6d8aa8
Ben Mossman <bdmossma@gmail.com>
2017-01-07 19:33:15 -0600

    reorganizing hello nodejs example

--- js/hellonodejs/app.js
+++ js/hellonodejs/app.js
@@ -2,6 +2,8 @@ var http = require("http");
 
 var jsonResponse = { "hello": "nodejs" };
 
+// To access the HTTP server, load this URL in your browser:
+// http://localhost:8080
 var app = http.createServer(function(httpRequest, httpResponse){
     httpResponse.setHeader("Content-Type", "application/json");
     httpResponse.end(JSON.stringify(jsonResponse));
