9a71225ccaa77f763ee2e6225411a1697ed4ee28
Ben Mossman <bdmossma@gmail.com>
2017-01-07 19:31:11 -0600

    reorganizing hello nodejs example

--- js/hellonodejs/app.js
+++ js/hellonodejs/app.js
@@ -3,8 +3,8 @@ var http = require("http");
 var jsonResponse = { "hello": "nodejs" };
 
 var app = http.createServer(function(httpRequest, httpResponse){
-	httpResponse.setHeader("Content-Type", "application/json");
-	httpResponse.end(JSON.stringify(jsonResponse));
+    httpResponse.setHeader("Content-Type", "application/json");
+    httpResponse.end(JSON.stringify(jsonResponse));
 });
 
 app.listen(8080);
