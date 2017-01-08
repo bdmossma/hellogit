adaa34ed684c75c1ae0a777807aa7bba0f1d5f4e
Ben Mossman <bdmossma@gmail.com>
2017-01-07 19:37:49 -0600

    reorganizing hello cs6js example

--- js/hellocs6js/app.js
+++ js/hellocs6js/app.js
@@ -1,27 +1,27 @@
 
 "use strict";
 
-var Animal = require("./animal.js").Animal;
-var Cat = require("./cat.js").Cat;
-var Dog = require("./dog.js").Dog;
-var Bird = require("./bird.js").Bird;
+var Animal = require("./animal").Animal;
+var Cat = require("./cat").Cat;
+var Dog = require("./dog").Dog;
+var Bird = require("./bird").Bird;
 
 
-// Do some work on the Animal base class
+// Do something with the Animal base class
 let animal = new Animal("Lassie");
 console.log("Created animal named: " + animal.name);
 
 animal.name = "Bingo";
 console.log("Renamed animal to: " + animal.name);
 
-// Do some work on the derived classes
+// Do something with the derived classes
 let cat = new Cat("Garfield");
 cat.talk();
 
 let dog = new Dog("Jock");
 dog.talk();
 
-// Do some work on a bad derived class
+// Do something withi a bad derived class
 // that forgot to implement an interface declared
 // in the base class.
 // This should throw an error.
