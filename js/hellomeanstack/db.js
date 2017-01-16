069134d815b3a3f8f4177dd81c3d8081a00fb055
Ben Mossman <bdmossma@gmail.com>
2017-01-16 17:22:23 -0600

    meanstack polish

---
+++ js/hellomeanstack/db.js
@@ -0,0 +1,21 @@
+
+//------------------------------------------------------------------------------
+// This module provides a library for doing CRUD operations
+// on a mongodb database.
+//------------------------------------------------------------------------------
+var db = require("mongodb");// native mongodb client
+
+function login(email, password, callback) {
+
+}
+
+function signup(email, password, callback) {
+
+}
+
+// TODO: Add functions for CRUD operations, then build the high level
+// functions like login and signup using these
+// low level functions.
+
+module.exports.login = login;
+module.exports.signup = signup;
--- js/hellomeanstack/login_api.js
+++ js/hellomeanstack/login_api.js
@@ -15,18 +15,23 @@ var db = require("db");
 // where PROJECT_ID=astral-sorter-155816
 // ==> https://astral-sorter-155816.appspot.com/apis/login
 //------------------------------------------------------------------------------
-router.get("/apis/login", function(httpRequest, httpResponse) {
+router.get("/apis/login/:email/:password", function(httpRequest, httpResponse) {
+    // Regarding Security: Yes, we are actually putting the password in the URL as
+    // this is just a fun little HTTP (plaintext) example.  Of course, in production,
+    // we would use Basic Authentication over HTTPS.
     var clientIp = httpRequest.headers['x-forwarded-for'] || httpRequest.connection.remoteAddress;
     console.log("Client " + clientIp + " is trying to login...");
 
-    if( "userId" && "bookId" in httpRequest.params) {
-        console.log("These URL params could be used to query data from a database for example.");
-        console.log("userId: " + httpRequest.params.userId);
-        console.log("bookId: " + httpRequest.params.bookId);
-    }
-
     var jsonResponse = {"hello": "api1"};
-    httpResponse.send(jsonResponse);
+
+    if( "email" && "password" in httpRequest.params) {
+        console.log("Performing login...");
+        db.login(httpRequest.params.email,
+                    httpRequest.params.password,
+                    function(error, status) {
+                        httpResponse.send(jsonResponse);
+                    });
+    }
 });
 
 module.exports = router
