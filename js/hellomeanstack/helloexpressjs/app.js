ef142a416a6e2088906ff316190a37573a86f1f4
Ben Mossman <bdmossma@gmail.com>
2017-01-18 18:00:48 -0600

    organizing hellomeanstack

--- js/hellomeanstack/helloexpressjs/app.js
+++ js/hellomeanstack/helloexpressjs/app.js
@@ -8,12 +8,28 @@ var express = require("express");
 var app = express();
 
 //------------------------------------------------------------------------------
+// Import CORS package so that ExpressJS can respond with CORS headers
+// to clients that are deployed out in the cloud for cross origin
+// resource sharing.
+// Per the wikipedia article:
+// CORS allows restricted resources on a web page to be requested from another
+// domain outside the domain from which the first resource was served.
+// A web page may freely embed cross-origin images, stylesheets, scripts,
+// iframes, and videos.
+//------------------------------------------------------------------------------
+var cors = require("cors");
+
+//------------------------------------------------------------------------------
 // Let's import modular APIs for login and signup from separate JavaScript files.
 // (We can omit the ".js" extension)
 //------------------------------------------------------------------------------
 var loginApi = require("./login_api");
 var signupApi = require("./signup_api");
 
+
+// inject APIs and plugins into
+// ExpressJS framework
+app.use(cors());
 app.use(loginApi);
 app.use(signupApi);
 
