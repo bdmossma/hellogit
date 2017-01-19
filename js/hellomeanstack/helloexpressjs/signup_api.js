7ee3aa9a46e753e2f4d7af4425fd3acf16316fc7
Ben Mossman <bdmossma@gmail.com>
2017-01-18 21:27:49 -0600

    greatly imporved helloexpressjs in hellomeanstack

--- js/hellomeanstack/helloexpressjs/signup_api.js
+++ js/hellomeanstack/helloexpressjs/signup_api.js
@@ -14,7 +14,7 @@ var mongoInfo = require("./mongodb_info");
 //------------------------------------------------------------------------------
 // URL: https://[PROJECT_ID][appspot.com]/apis/signunp
 // where PROJECT_ID=astral-sorter-155816
-// ==> https://astral-sorter-155816.appspot.com/apis/signup
+// ==> https://astral-sorter-155816.appspot.com/apis/signUp
 //------------------------------------------------------------------------------
 router.get("/apis/signup/:firstName/:lastName/:phone/:email/:password", function(httpRequest, httpResponse) {
     // Regarding Security: Yes, we are actually putting the password in the URL as
