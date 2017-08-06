6e282a4cd346c657b5da8fbd92f3595a883ed5bf
Ben Mossman <bdmossma@gmail.com>
2017-08-06 09:17:05 -0500

    one step at a time

--- js/hellopoormancdn/www/js/clientApp.js
+++ js/hellopoormancdn/www/js/clientApp.js
@@ -1,6 +1,6 @@
 angular.module('app', [], function() {});
-FileUploadController.$inject = ['$scope'];
-function FileUploadController(scope) {
+FileUploadController.$inject = ['$scope', '$http'];
+function FileUploadController(scope, http) {
     //============== DRAG & DROP =============
     // source for drag&drop: http://www.webappers.com/2011/09/28/drag-drop-file-upload-with-html5-javascript/
     var dropbox = document.getElementById("dropbox");
@@ -44,16 +44,46 @@ function FileUploadController(scope) {
 					var file = files[i];
 					file.progressVisible = false;
 					files.progress = 0;
-					files.status = "pending";
+					files.status = "not uploaded";
                     scope.files.push(file);
+					//var testFile = { "name" : file.name, "size" : file.size, "progressVisible" : false, "progress" : 0, "status" : "uploaded"};
+					//scope.files.push(testFile);
                 }
-            })
+            });
         }
+
+		scope.listUploadedFiles();
+
 		//immediately start uploading files upon dropping them
 		scope.uploadFiles();
     }, false);
     //============== DRAG & DROP =============
 
+	scope.listUploadedFiles = function() {
+	    scope.$apply(function() {
+			// Use the users API which has been deployed
+	        // in the Amazon Cloud
+	        var filesApi = "/apis/files/";//this URL is relative to base URL
+	        var httpResponse = http.get(filesApi);
+
+	        // $http parses json for us, so we can just use it without having to parse it out
+	        httpResponse.success( function(files) {
+	            console.log(JSON.stringify(files));
+	            scope.$apply(function(){
+		            for(var fileIndex in files){
+						var file = new File([""], files[fileIndex].name, {});
+						file.size = files[fileIndex].size;
+						file.progressVisible = false;
+						files.progress = 0;
+						files.status = "uploaded";
+	                    scope.files.push(file);
+		            }
+				});
+	        });
+		});
+	}
+
+	/*
     scope.setFiles = function(element) {
     scope.$apply(function(scope) {
 			// Initialize file listing as an empty array of json objects
@@ -69,6 +99,7 @@ function FileUploadController(scope) {
 			scope.progressVisible = false;
 		});
     };
+	*/
 
     scope.uploadFiles = function() {
         for (var fileIndex in scope.files) {
@@ -82,6 +113,7 @@ function FileUploadController(scope) {
 	        xhr.addEventListener("abort", uploadCanceled, false);
 	        xhr.open("POST", "/apis/upload");
 	        scope.files[fileIndex].progressVisible = true;
+			scope.files[fileIndex].status = "uploading";
 	        xhr.send(formData);
 		}
     }
@@ -100,14 +132,19 @@ function FileUploadController(scope) {
         scope.$apply(function(){
         /* This event is raised when the server send back a response */
 		scope.files[event.target.fileIndex].progressVisible = false;
-		scope.files[event.target.fileIndex].status = event.target.responseText;
+		scope.files[event.target.fileIndex].status = "uploaded";//event.target.responseText;
 	})
     }
 
     function uploadFailed(event) {
         scope.$apply(function(){
 			scope.files[event.target.fileIndex].progressVisible = false;
-			scope.files[event.target.fileIndex].status = event.target.responseText;
+			// don't display error for failed file upload, rather, must remove it
+			// from the file listing
+			var fileIndexToRemove = event.target.fileIndex;
+			var numberOfFileIndicesToRemove = 1;//just remove one item from this position
+			scope.files.splice(fileIndexToRemove, numberOfFileIndicesToRemove);
+			//scope.files[event.target.fileIndex].status = event.target.responseText;
 		});
     }
 
